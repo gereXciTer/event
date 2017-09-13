7883f6c0da29a655ce521ad01f1eaba1732988db
Vladimir Gartsev <vladimir.gartsev@gmail.com>
2017-09-13 01:20:57 -0400

    Adopting to deployment

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/eventdb');
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://split.webgears.org");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cookie");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
