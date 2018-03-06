d3062dfd2ed5dd3684eb1de64127899259221345
unknown <vladimir.gartsev@gmail.com>
2018-03-05 23:29:04 -0500

    using sparkpost instead of nodemailer

--- server/api/controllers/emailController.js
+++ server/api/controllers/emailController.js
@@ -1,16 +1,46 @@
-const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport');
-const transporter = nodemailer.createTransport(smtpTransport({
-        host: 'localhost',
-        port: 587,
-        secure: false,
-        ignoreTLS: true
-      }));
+const SparkPost = require('sparkpost');
+const client = new SparkPost();
+
+
+
+// const nodemailer = require('nodemailer');
+// const smtpTransport = require('nodemailer-smtp-transport');
+// const transporter = nodemailer.createTransport(smtpTransport({
+//         host: 'localhost',
+//         port: 587,
+//         secure: false,
+//         ignoreTLS: true
+//       }));
 
 exports.address = {
-    noreply: 'noreply@webgears.org'
+	noreply: 'noreply@mail.webgears.org'
 }
-exports.transporter = transporter
+exports.transporter = {
+	sendMail: function (options, cb) {
+		client.transmissions.send({
+			options: {
+				sandbox: false
+			},
+			content: {
+				from: options.from,
+				subject: options.subject,
+				html: options.html
+			},
+			recipients: [
+				{address: options.to}
+			]
+		})
+		.then(data => {
+			if(cb && typeof cb == 'function') {
+				cb(data);
+			}
+		})
+		.catch(err => {
+			console.log('Whoops! Something went wrong');
+			console.log(err);
+		});
+	}
+}
 exports.getHostUrl = function (req) {
-    return req.protocol + '://' + req.get('host');
+	return req.protocol + '://' + req.get('host');
 }
